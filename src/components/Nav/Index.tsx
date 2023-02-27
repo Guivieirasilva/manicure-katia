90c204a6127efba11f2b65db62b1bc8223f315e4
gui vieira <guivieirasilva02@gmail.com>
2023-02-27 00:04:24 -0300

    nav bar init

--- src/components/Nav/Index.tsx
+++ src/components/Nav/Index.tsx
@@ -1,14 +1,27 @@
 import { BubblyLink } from "react-bubbly-transitions";
+import "./styles.css";
+import Menu from "../../assets/menu.svg";
 
 export default function Nav() {
   return (
-    <header>
-    <h1>Katia Manicure</h1>
-    <nav className="animate-in" style={{ animationDelay: "700ms" }}>
-      <BubblyLink to="About">Sobre</BubblyLink>
-      <BubblyLink to="Works" >Trabalhos</BubblyLink>
-      <BubblyLink to="Contact">Contato</BubblyLink>
-    </nav>
+    <header className="headerNav">
+      <BubblyLink to="/">
+        <h1 style={{ letterSpacing: 6, color: "white" }}>Katia Manicure</h1>
+      </BubblyLink>
+      <nav
+        className="animate-in navbar"
+        style={{ animationDelay: "700ms", fontWeight: 600 }}
+      >
+        <BubblyLink to="About">Sobre</BubblyLink>
+        <BubblyLink to="Works">Trabalhos</BubblyLink>
+        <BubblyLink to="Contact">Contato</BubblyLink>
+      </nav>
+      <button
+        className="menu"
+        style={{ backgroundColor: "#9d3dc8", border: "none" }}
+      >
+        <img width={32}  src={Menu} />
+      </button>
     </header>
   );
 }
--- src/pages/About/Index.tsx
+++ src/pages/About/Index.tsx
@@ -3,6 +3,7 @@ export default function About() {
   return (
     <>
       <header>
+        <p>About</p>
       </header>
     </>
   );
