18c848ea78bbacc7230d1083fa7e090316aa92d6
gui vieira <guivieirasilva02@gmail.com>
2023-03-18 22:18:41 -0300

    descrição de /Sobre

--- src/pages/About/Index.tsx
+++ src/pages/About/Index.tsx
@@ -1,10 +1,26 @@
+import { Container, Text, Title } from "./styles";
 
 export default function About() {
   return (
     <>
-      <header>
-        <p>About</p>
-      </header>
+      <Container>
+        <Title>
+          <p>Sobre</p>
+        </Title>
+
+        <Text>
+          <p>
+            Katia é uma manicure experiente e altamente qualificada, que oferece
+            serviços personalizados de cuidados das unhas das mãos e dos pés,
+            utilizando apenas produtos de alta qualidade e esterilizados. Com
+            atenção aos detalhes e cuidado, ela busca proporcionar uma
+            experiência relaxante e agradável para seus clientes, garantindo que
+            suas unhas fiquem saudáveis, bonitas e bem cuidadas. Se você está em
+            busca de um serviço de qualidade, personalizado e com um preço
+            acessível, agende agora mesmo seu horário com a Katia.
+          </p>
+        </Text>
+      </Container>
     </>
   );
 }
