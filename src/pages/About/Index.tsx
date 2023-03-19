8d0f448100a78b32aeac8c484dd0d5200b5cc56d
gui vieira <guivieirasilva02@gmail.com>
2023-03-18 22:45:17 -0300

    conteudo da tela de sobre

--- src/pages/About/Index.tsx
+++ src/pages/About/Index.tsx
@@ -1,4 +1,9 @@
-import { Container, Text, Title } from "./styles";
+import { Container, ContainerMetodologia, Text, Title } from "./styles";
+
+const Images = [unhas, unhas, unhas, unhas, unhas, unhas];
+import unhas from "../../assets/unhas.jpg";
+import { Carousel } from "../../components/Carousel/Index";
+import { CarouselContainer } from "./styles";
 
 export default function About() {
   return (
@@ -21,6 +26,60 @@ export default function About() {
           </p>
         </Text>
       </Container>
+
+      <div
+        style={{
+          width: "100%",
+          backgroundColor: "#8e38b5",
+          height: "600px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: '20px',
+          marginBottom: '50px'
+        }}
+      >
+        <CarouselContainer>
+          <Carousel images={Images} />
+        </CarouselContainer>
+      </div>
+
+      <ContainerMetodologia>
+        <h3>Metodologia de Trabalho</h3>
+        <p>
+          A metodologia de trabalho de uma manicure é essencial para garantir um
+          resultado final de qualidade e satisfação dos clientes. Katia Lima,
+          uma experiente manicure, possui uma abordagem de trabalho que é
+          baseada em técnicas precisas e aperfeiçoadas ao longo de mais de uma
+          década de prática. Em primeiro lugar, Katia sempre inicia o trabalho
+          com uma avaliação das unhas de seus clientes. Essa etapa é fundamental
+          para entender as necessidades e preferências de cada pessoa, assim
+          como identificar possíveis problemas que possam afetar o resultado
+          final.
+          <span className="span">
+            {" "}
+            A partir daí, ela define qual será o tipo de tratamento ou decoração
+            a ser realizado. Em seguida, Katia inicia a preparação das unhas,
+            que inclui a remoção de cutículas, limpeza, lixamento e polimento.
+            Essa etapa é importante para garantir que as unhas fiquem uniformes
+            e prontas para receber o tratamento ou decoração escolhidos. Para a
+            decoração das unhas, Katia utiliza diversas técnicas, como pintura à
+            mão livre, aplicação de adesivos, carimbos e pedrarias. Ela sempre
+            busca entender o estilo e gosto de cada cliente para criar um visual
+            personalizado e exclusivo. Além disso, ela utiliza materiais de alta
+            qualidade para garantir que a decoração tenha durabilidade e
+            resistência. Por fim, Katia finaliza o trabalho com uma camada de
+            finalizador para proteger e dar brilho às unhas. Ela também oferece
+            dicas e cuidados para que os clientes possam manter as unhas bonitas
+            e saudáveis por mais tempo. Em resumo, a metodologia de trabalho de
+            Katia Lima é baseada em uma abordagem personalizada e técnica, que
+            visa garantir um resultado final de qualidade e satisfação dos
+            clientes. Sua experiência e aperfeiçoamento constante em diversas
+            áreas da decoração de unhas garantem um trabalho impecável e de
+            excelência.
+          </span>
+        </p>
+      </ContainerMetodologia>
     </>
   );
 }
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -7,10 +7,11 @@ export const Container = styled.div`
   justify-content: space-around;
   align-items: center;
   padding: 50px;
+  height: 500px;
 
   @media screen and (max-width: 640px) {
     flex-direction: column;
-    padding: 10px 20px;
+    padding: 10px 20px 40px 10px;
   }
 `;
 
@@ -24,7 +25,7 @@ export const Title = styled.div`
   justify-content: center;
   font-weight: 500;
   @media screen and (max-width: 640px) {
-   margin-top: 20px;
+    margin-top: 20px;
     height: 100px;
   }
 `;
@@ -41,13 +42,73 @@ export const Text = styled.div`
   }
 
   @media screen and (max-width: 640px) {
-   width: 400px;
+    width: 400px;
   }
   @media screen and (max-width: 500px) {
-   width: 300px;
-   padding: 5px;
-   
+    width: 300px;
+    padding: 5px;
+  }
+`;
 
+export const CarouselContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 1200px;
+  width: "70%";
+  height: 320px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0px 10px;
+  background-color: var(--primaryDark);
+
+  @media screen and (max-width: 1350px) {
+    max-width: 1000px;
+  }
+  @media screen and (max-width: 1150px) {
+    max-width: 850px;
+  }
+  @media screen and (max-width: 1000px) {
+    max-width: 650px;
+  }
+  @media screen and (max-width: 800px) {
+    max-width: 550px;
+  }
+  @media screen and (max-width: 650px) {
+    max-width: 300px;
+  }
+  @media screen and (max-width: 450px) {
+    max-width: 250px;
+  }
+`;
+
+export const ContainerMetodologia = styled.section`
+  display: flex;
+  width: "90%";
+  height: 500px;
+  margin-top: 50px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 30px 100px;
+  gap: 5px;
+  > h3 {
+    font-family: "Great Vibes", cursive;
+    letter-spacing: 4px;
+    font-size: 3rem;
+    font-weight: 500;
+    text-align: center;
+  }
+  > p {
+    text-align: center;
+    width: "80%";
+    font-size: 1.5rem;
+    padding: 10px 20px;
   }
 
+  @media screen and (max-width: 840px) {
+    .span {
+      display: none;
+    }
+  }
 `;
