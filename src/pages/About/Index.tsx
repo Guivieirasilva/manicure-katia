fccbc69ef9024e530b8ae6b0926a748eb117a9e5
gui vieira <guivieirasilva02@gmail.com>
2023-03-19 23:33:52 -0300

    finalizado tela about

--- src/pages/About/Index.tsx
+++ src/pages/About/Index.tsx
@@ -1,26 +1,29 @@
 import {
+  Box,
   Container,
-  ContainerGeral,
-  ContainerMetodologia,
+  ContainerCarousel,
+  Methodology,
+  PersonAbout,
   SubTitle,
   Text,
-  Title,
 } from "./styles";
 
 const Images = [unhas, unhas, unhas, unhas, unhas, unhas];
 import unhas from "../../assets/unhas.jpg";
 import { Carousel } from "../../components/Carousel/Index";
 import { CarouselContainer } from "./styles";
+import Person from "../../assets/image-about.png";
 
 export default function About() {
   return (
-    <ContainerGeral>
+    <Box>
       <Container>
-        <Title>
-          <p>Sobre</p>
-        </Title>
+        <PersonAbout>
+          <img src={Person} alt="Ilustração de pessoa" />
+        </PersonAbout>
 
         <Text>
+          <h3>Conheça a História da Katia!</h3>
           <p>
             Katia é uma manicure experiente e altamente qualificada, que oferece
             serviços personalizados de cuidados das unhas das mãos e dos pés,
@@ -33,31 +36,17 @@ export default function About() {
           </p>
         </Text>
       </Container>
-
-      <div
-        style={{
-          width: "100%",
-          backgroundColor: "#8e38b5",
-          height: "600px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "20px",
-          marginBottom: "50px",
-        }}
-      >
+      <ContainerCarousel>
         <SubTitle>
-          <h3>Ambiente de Trabalho</h3>
+          <h3>Veja o Ambiente de Trabalho!</h3>
         </SubTitle>
         <CarouselContainer>
           <Carousel images={Images} />
         </CarouselContainer>
-      </div>
-
-      <ContainerMetodologia>
-        <h3>Metodologia de Trabalho</h3>
-        <p>
+      </ContainerCarousel>
+      <Methodology>
+        <h3>Nosso Método de Cuidado com suas Unhas!</h3>
+        <p className="desktop" >
           A metodologia de trabalho de uma manicure é essencial para garantir um
           resultado final de qualidade e satisfação dos clientes. Katia Lima,
           uma experiente manicure, possui uma abordagem de trabalho que é
@@ -72,24 +61,34 @@ export default function About() {
           polimento. Essa etapa é importante para garantir que as unhas fiquem
           uniformes e prontas para receber o tratamento ou decoração escolhidos.
           Para a decoração das unhas, Katia utiliza diversas técnicas, como
-          pintura à mão livre, aplicação de adesivos, carimbos e pedrarias.{" "}
-          <span className="span">
-            {" "}
-            Ela sempre busca entender o estilo e gosto de cada cliente para
-            criar um visual personalizado e exclusivo. Além disso, ela utiliza
-            materiais de alta qualidade para garantir que a decoração tenha
-            durabilidade e resistência. Por fim, Katia finaliza o trabalho com
-            uma camada de finalizador para proteger e dar brilho às unhas. Ela
-            também oferece dicas e cuidados para que os clientes possam manter
-            as unhas bonitas e saudáveis por mais tempo. Em resumo, a
-            metodologia de trabalho de Katia Lima é baseada em uma abordagem
-            personalizada e técnica, que visa garantir um resultado final de
-            qualidade e satisfação dos clientes. Sua experiência e
-            aperfeiçoamento constante em diversas áreas da decoração de unhas
-            garantem um trabalho impecável e de excelência.
-          </span>
+          pintura à mão livre, aplicação de adesivos, carimbos e pedrarias. Ela
+          sempre busca entender o estilo e gosto de cada cliente para criar um
+          visual personalizado e exclusivo. Além disso, ela utiliza materiais de
+          alta qualidade para garantir que a decoração tenha durabilidade e
+          resistência. Por fim, Katia finaliza o trabalho com uma camada de
+          finalizador para proteger e dar brilho às unhas. Ela também oferece
+          dicas e cuidados para que os clientes possam manter as unhas bonitas e
+          saudáveis por mais tempo. Em resumo, a metodologia de trabalho de
+          Katia Lima é baseada em uma abordagem personalizada e técnica, que
+          visa garantir um resultado final de qualidade e satisfação dos
+          clientes. Sua experiência e aperfeiçoamento constante em diversas
+          áreas da decoração de unhas garantem um trabalho impecável e de
+          excelência.
         </p>
-      </ContainerMetodologia>
-    </ContainerGeral>
+          <p className="mobile">
+            A metodologia de trabalho de Katia Lima, uma experiente manicure, é
+            baseada em uma abordagem personalizada e técnica. Ela começa
+            avaliando as unhas do cliente para entender suas necessidades e
+            preferências, além de identificar possíveis problemas. Em seguida,
+            prepara as unhas para receber o tratamento ou decoração escolhidos,
+            utilizando materiais de alta qualidade e diversas técnicas, como
+            pintura à mão livre, aplicação de adesivos, carimbos e pedrarias.
+            Finaliza o trabalho com uma camada de finalizador para proteger e
+            dar brilho às unhas, além de oferecer dicas e cuidados para
+            mantê-las bonitas e saudáveis. A metodologia de Katia garante um
+            resultado final de qualidade e satisfação dos clientes.
+          </p>
+      </Methodology>
+    </Box>
   );
 }
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,58 +1,110 @@
 import styled from "styled-components";
 
-export const ContainerGeral = styled.div`
+export const Box = styled.div`
   display: flex;
   flex-direction: column;
+  background-color: var(--primaryDark);
   gap: 10px;
 `;
+
+/* Header */
 
 export const Container = styled.div`
   width: "100%";
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
-  padding: 50px;
+  padding: 20px 50px;
   height: 500px;
-
+  border-bottom: 0.1rem solid purple;
   @media screen and (max-width: 640px) {
     flex-direction: column;
     padding: 10px 20px 40px 10px;
   }
 `;
 
-export const Title = styled.div`
-  width: 400px;
-  font-family: "Libre Baskerville", serif;
-  font-weight: 700;
-  font-size: 2.5rem;
+export const PersonAbout = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  @media screen and (max-width: 640px) {
-    margin-top: 20px;
-    height: 100px;
+  align-self: flex-end;
+  max-width: 400px;
+  width: 40%;
+  > img {
+    width: 100%;
+  }
+  @media screen and (max-width: 750px) {
+    display: none;
   }
 `;
 
 export const Text = styled.div`
-  width: 750px;
-  font-family: "inter", sans-serif;
+  background-color: rgba(157, 61, 200, 0.3);
+  color: var(--textColor);
+  text-shadow: black 0.1em 0.1em 0.2em;
+  max-width: 600px;
   font-size: 1.3rem;
   display: flex;
+  flex-direction: column;
+  width: 80%;
   align-items: center;
   justify-content: center;
+  > h3 {
+    font-family: "Libre Baskerville", serif;
+    font-weight: 700;
+    font-size: 1.6rem;
+    text-align: left;
+  }
   > p {
-    text-align: center;
+    text-align: justify;
+    padding: 0 20px;
+    font-size: 1rem;
+  }
+  @media screen and (max-width: 900px) {
+    width: 100%;
+    > h3 {
+      font-size: 1.5rem;
+    }
   }
 
-  @media screen and (max-width: 640px) {
-    width: 400px;
-    font-size: 1.1rem;
+  @media screen and (max-width: 400px) {
+    > h3 {
+      font-size: 1.3rem;
+    }
   }
-  @media screen and (max-width: 500px) {
-    width: 300px;
-    padding: 5px;
+`;
+
+/* Carousel */
+
+export const ContainerCarousel = styled.div`
+  width: 100%;
+  background-color: #8e38b5;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
+export const SubTitle = styled.div`
+  font-size: 2.5rem;
+  display: flex;
+  align-items: "center";
+  justify-content: "center";
+  > h3 {
+    color: var(--textColor);
+    text-shadow: black 0.1em 0.1em 0.2em;
+    display: flex;
+    margin: 10px auto;
+    letter-spacing: 5px;
+    font-weight: 500;
+    text-align: center;
+    font-size: 2rem;
+  }
+  @media screen and (max-width: 660px) {
+    > h3 {
+      font: 1.5rem;
+    }
   }
 `;
 
@@ -60,60 +112,67 @@ export const CarouselContainer = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 80%;
   max-width: 1200px;
-  width: "70%";
   height: 320px;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 0px 10px;
+  margin: 0 auto;
+  padding: 0 10px;
   background-color: var(--primaryDark);
 
-  @media screen and (max-width: 1350px) {
+  @media screen and (max-width: 1350px) and (min-width: 1151px) {
     max-width: 1000px;
   }
-  @media screen and (max-width: 1150px) {
+
+  @media screen and (max-width: 1150px) and (min-width: 1001px) {
     max-width: 850px;
   }
-  @media screen and (max-width: 1000px) {
+
+  @media screen and (max-width: 1000px) and (min-width: 801px) {
     max-width: 650px;
   }
-  @media screen and (max-width: 800px) {
+
+  @media screen and (max-width: 800px) and (min-width: 651px) {
     max-width: 550px;
   }
-  @media screen and (max-width: 650px) {
-    max-width: 300px;
+
+  @media screen and (max-width: 650px) and (min-width: 451px) {
+    max-width: 350px;
   }
+
   @media screen and (max-width: 450px) {
     max-width: 250px;
   }
 `;
 
-export const ContainerMetodologia = styled.section`
-  margin: 0 auto;
+export const Methodology = styled.article`
+  background-color: var(--background);
   display: flex;
-  max-width: 100rem;
-  width: "80%";
-  height: 650px;
+  width: 100%;
+  height: 800px;
   margin-top: 50px;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 30px 100px;
+  /* padding: 30px 100px; */
   gap: 5px;
   > h3 {
     font-family: "Libre Baskerville", serif;
     font-weight: 700;
-    font-size: 2rem;
+    font-size: 1.8rem;
     text-align: center;
   }
   > p {
-    text-align: center;
-    width: "90%";
-    font-size: 1.5rem;
+    text-align: justify;
+    max-width: 1000px;
+    width: 90%;
+    font-size: 1.2rem;
     padding: 0px 20px;
   }
 
-  @media screen and (max-width: 900px) {
+  .mobile {
+    display: none;
+  }
+  @media screen and (max-width: 1100px) {
     width: "70%";
     padding: 30px;
     > p {
@@ -122,30 +181,14 @@ export const ContainerMetodologia = styled.section`
     }
   }
 
-  @media screen and (max-width: 840px) {
-    .span {
+  @media screen and (max-width: 800px) {
+    height: 750px;
+
+    > .desktop {
       display: none;
     }
-  }
-`;
-
-export const SubTitle = styled.div`
-  font-family: "Great Vibes", cursive;
-  font-size: 2.5rem;
-  display: flex;
-  align-items: "center";
-  justify-content: "center";
-  > h3 {
-    color: var(--textColor);
-    display: flex;
-    margin: 10px auto;
-    letter-spacing: 5px;
-    font-weight: 500;
-    text-align: center;
-  }
-  @media screen and (max-width: 840px) {
-    > h3 {
-      font-size: 2rem;
+    > .mobile {
+      display: block;
     }
   }
 `;
