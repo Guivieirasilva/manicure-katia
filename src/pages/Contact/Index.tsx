fe83f755e9211a41c754cf6ddc7b82dbccecb453
gui vieira <guivieirasilva02@gmail.com>
2023-03-26 17:26:07 -0300

    pagina de contato

--- src/pages/Contact/Index.tsx
+++ src/pages/Contact/Index.tsx
@@ -1,10 +1,146 @@
+import { useState } from "react";
+import { Container, Form, Label, Localization, SocialMedias } from "./styles";
+
+import facebook from "../../assets/iconFacebook.svg";
+import whatsapp from "../../assets/iconsWhatsapp.svg";
+import instagram from "../../assets/iconInstagram.svg";
+
+const linkFacebook = "https://www.facebook.com/katia.limagoncalves";
+const linkInstagram = "https://www.instagram.com/kkatia_lima/";
+const linkWhatsapp = "https://www.whatsapp.com/";
 
 export default function Contact() {
+  const [inputsForm, setInputsForm] = useState({
+    nome: "",
+    email: "",
+    telefone: "",
+    assunto: "",
+    duvida: "",
+  });
+
   return (
-    <>
-    <header>
-      <p>CONTACT</p>
-    </header>
-    </>
-  )
+    <Container>
+      <Localization>
+        <h3>Localização do Ambiente</h3>
+        <div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1830.3241150263561!2d-46.517536702650006!3d-23.437071099999987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef52cb9db51dd%3A0x3c9f2cf09068def3!2sR.%20Araraquara%2C%20115%20-%20Jardim%20Bela%20Vista%2C%20Guarulhos%20-%20SP%2C%2007132-300!5e0!3m2!1spt-BR!2sbr!4v1679852967995!5m2!1spt-BR!2sbr"
+            loading="lazy"
+          ></iframe>
+        </div>
+      </Localization>
+      <SocialMedias>
+        <h2>Redes Sociais</h2>
+        <div>
+          <a href={linkFacebook} target="_blank">
+            <img src={facebook} alt="Rede Social" />{" "}
+          </a>
+          <a href={linkInstagram} target="_blank">
+            <img src={instagram} alt="Rede Social" />{" "}
+          </a>
+          <a href={linkWhatsapp} target="_blank">
+            <img src={whatsapp} alt="Rede Social" />{" "}
+          </a>
+        </div>
+      </SocialMedias>
+
+      <Form>
+        <h2>Entre em Contato</h2>
+        <form>
+          <Label htmlFor="nome">
+            Nome Completo:
+            <input
+              value={inputsForm.nome}
+              onChange={(e) =>
+                setInputsForm({
+                  ...inputsForm,
+                  nome: e.target.value,
+                })
+              }
+              placeholder=" Nome Completo"
+              type="text"
+              id="nome"
+              name="nome"
+              required
+            />
+          </Label>
+
+          <Label htmlFor="email">
+            E-mail:
+            <input
+              value={inputsForm.email}
+              onChange={(e) =>
+                setInputsForm({
+                  ...inputsForm,
+                  email: e.target.value,
+                })
+              }
+              type="email"
+              placeholder=" Digite o Email"
+              id="email"
+              name="email"
+              required
+            />
+          </Label>
+
+          <Label htmlFor="telefone">
+            Telefone:
+            <input
+              value={inputsForm.telefone}
+              onChange={(e) =>
+                setInputsForm({
+                  ...inputsForm,
+                  telefone: e.target.value,
+                })
+              }
+              placeholder=" (11) 99999-9999"
+              type="tel"
+              id="telefone"
+              name="telefone"
+              required
+            />
+          </Label>
+
+          <Label htmlFor="assunto">
+            Assunto:
+            <input
+              value={inputsForm.assunto}
+              onChange={(e) =>
+                setInputsForm({
+                  ...inputsForm,
+                  assunto: e.target.value,
+                })
+              }
+              type="text"
+              placeholder=" Digite o assunto"
+              id="assunto"
+              name="assunto"
+              required
+            />
+          </Label>
+
+          <Label htmlFor="duvida">
+            Dúvida:
+            <textarea
+              value={inputsForm.duvida}
+              onChange={(e) =>
+                setInputsForm({
+                  ...inputsForm,
+                  duvida: e.target.value,
+                })
+              }
+              id="duvida"
+              placeholder=" Digite sua duvida"
+              name="duvida"
+              required
+            ></textarea>
+          </Label>
+
+          <button type="submit" onChange={(e) => e.preventDefault()}>
+            Enviar
+          </button>
+        </form>
+      </Form>
+    </Container>
+  );
 }
---
+++ src/pages/Contact/styles.ts
@@ -0,0 +1,149 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: var(--primaryDark);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+export const Localization = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 500px;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
+  color: var(--textColor);
+  text-shadow: black 0.1em 0.1em 0.2em;
+  font-size: 1.5rem;
+  padding: 0 20px 50px 20px;
+
+  > div {
+    max-width: 700px;
+    max-height: 400px;
+    width: 80%;
+    height: 90%;
+    border: 1px solid purple;
+    background-color: purple;
+    border-radius: 20px;
+
+    > iframe {
+      width: 100%;
+      height: 100%;
+      border-radius: 20px;
+    }
+  }
+`;
+
+export const SocialMedias = styled.section`
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 100%;
+  height: 500px;
+  background-color: var(--background);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  > h2 {
+    font-size: 1.8rem;
+    font-family: "Libre Baskerville", serif;
+  }
+  > div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+    flex-direction: row;
+
+    > a {
+      transition-duration: 0.8s;
+      :hover {
+        transform: scale(1.1, 1.1);
+      }
+      img {
+        width: 200px;
+      }
+    }
+  }
+
+  @media screen and (max-width: 680px) {
+    > div {
+      gap: 5px;
+      flex-direction: column;
+      > a {
+        img {
+          width: 120px;
+        }
+      }
+    }
+  }
+`;
+
+export const Form = styled.div`
+  display: flex;
+  font-size: 1.2rem;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
+  color: var(--textColor);
+  text-shadow: black 0.1em 0.1em 0.2em;
+  font-family: "Libre Baskerville", serif;
+  padding-bottom: 60px;
+  padding-top: 10px;
+
+  border-bottom: 1px solid purple;
+  > form {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    gap: 15px;
+    align-items: center;
+    justify-content: center;
+    font-family: "inter", sans-serif;
+    > button {
+      width: 150px;
+      height: 40px;
+      border: 2px solid;
+      border-color: var(--primary);
+      border-radius: 30px;
+      background-color: var(--textColor);
+      color: var(--primary);
+      font-family: "inter", sans-serif;
+      font-weight: 600;
+      transition-duration: 0.4s;
+      text-transform: uppercase;
+      :hover {
+        background-color: var(--primaryDark);
+        color: var(--textColor);
+        border-color: var(--textColor);
+      }
+    }
+  }
+`;
+export const Label = styled.label`
+  max-width: 800px;
+  width: 80%;
+  > textarea {
+    font-size: 1rem;
+    letter-spacing: 2px;
+    width: 100%;
+    height: 100px;
+    margin-left: 10px;
+    border-radius: 5px;
+  }
+  > input {
+    font-size: 1rem;
+    letter-spacing: 2px;
+    width: 100%;
+    margin-left: 10px;
+    height: 20px;
+    border-radius: 5px;
+    border: none;
+    padding: 5px;
+  }
+`;
