1290fff4e36320901239f2e5103e6403c1b2569a
gui vieira <guivieirasilva02@gmail.com>
2023-03-26 14:06:40 -0300

    tela de trabalhos finalizada

--- src/pages/Works/Index.tsx
+++ src/pages/Works/Index.tsx
@@ -1,25 +1,55 @@
 import { CarouselLarge } from "../../components/CarouselLarge/Index";
 
-import ambiente1 from "../../assets/ambiente1.jpg"
-import ambiente2 from "../../assets/ambiente2.jpg"
+import unhas from "../../assets/unhas.jpg";
+import unhaDecorada from "../../assets/unha-decorada.jpg";
 
-const images = [ambiente1,ambiente2,ambiente2,ambiente1] 
+import {
+  Box,
+  CarouselContainer,
+  Container,
+  Image,
+  SubTitle,
+  Techniques,
+  Text,
+} from "./styles";
+import { Carousel } from "../../components/Carousel/Index";
+const Images = [unhas, unhaDecorada, unhas, unhaDecorada, unhas, unhaDecorada];
 
 export default function Works() {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "500px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: 'black'
-        
+    <Container>
+      <Box>
+        <SubTitle>
+          <h3>Unhas que Vão te Surpreender</h3>
+        </SubTitle>
+        <CarouselContainer>
+          <Carousel images={Images} />
+        </CarouselContainer>
+      </Box>
 
-      }}
-    >
-      <CarouselLarge images={images} />
-    </div>
+      <Techniques>
+        <Text>
+          <div>
+            <h2>Beleza em suas Mãos {"\n"}<br/>
+            <span>
+              Técnicas Inovadoras para Unhas Incríveis!
+            </span>
+            </h2>
+          </div>
+          <p>
+            Katia é uma manicure especializada em unhas decoradas, oferecendo
+            uma variedade de técnicas para deixar suas unhas incríveis. Entre as
+            opções disponíveis estão a francesinha tradicional, a francesinha
+            invertida, a inglesinha, a filha única, o degradê e a decoração com
+            adesivos. Seja para uma ocasião especial ou para o dia a dia, Katia
+            oferece um serviço personalizado para cada cliente, utilizando
+            apenas os melhores produtos do mercado para garantir um acabamento
+            perfeito e duradouro. Agende um horário com ela para ter unhas
+            deslumbrantes e cheias de estilo!
+          </p>
+        </Text>
+        <Image><img src={unhaDecorada} alt=" imagem de Unha" /></Image>
+      </Techniques>
+    </Container>
   );
 }
